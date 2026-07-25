df2d957ab4d35e7c9c4b3282a6d95a316dd56d3d
M.gharaee <mahangharaee130@gmail.com>
2026-07-25 13:33:53 +0330

    ورژن جدید متد پریزما همراه با پجینیشن

--- apps/backend/infrastructure/context/dbContext/prisma/client.ts
+++ apps/backend/infrastructure/context/dbContext/prisma/client.ts
@@ -1,3 +1,8 @@
+/**
+ * This Version works Successfully.
+ */
+
+/*
 import { PrismaClient } from '@prisma/client';
 import env from '#substructure/env.ts';
 
@@ -6,6 +11,24 @@ const prismaClient = new PrismaClient({
 });
 
 const prisma = prismaClient;
+
+export type Prisma = typeof prisma;
+
+export default prisma;*/
+
+/**
+ * Let's try this version with pagination.
+ */
+
+import { PrismaClient } from '@prisma/client';
+import { pagination } from "prisma-extension-pagination";
+import env from '#substructure/env.ts';
+
+const prismaClient = new PrismaClient({
+    errorFormat: env.environment === 'production' ? 'minimal' : 'pretty',
+});
+
+const prisma = prismaClient.$extends(pagination());
 
 export type Prisma = typeof prisma;
 
