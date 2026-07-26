4995d57bb69bd8e11ad4066f868787707f0e662c
M.gharaee <mahangharaee130@gmail.com>
2026-07-26 15:29:03 +0330

    بهبود پریزما

--- apps/backend/infrastructure/context/dbContext/prisma/client.ts
+++ apps/backend/infrastructure/context/dbContext/prisma/client.ts
@@ -1,24 +1,3 @@
-/**
- * This Version works Successfully.
- */
-
-/*
-import { PrismaClient } from '@prisma/client';
-import env from '#substructure/env.ts';
-
-const prismaClient = new PrismaClient({
-    errorFormat: env.environment === 'production' ? 'minimal' : 'pretty',
-});
-
-const prisma = prismaClient;
-
-export type Prisma = typeof prisma;
-
-export default prisma;*/
-
-/**
- * Let's try this version with pagination.
- */
 import { PrismaClient } from '@prisma/client';
 import { pagination } from "prisma-extension-pagination";
 import env from '#substructure/env.ts';
