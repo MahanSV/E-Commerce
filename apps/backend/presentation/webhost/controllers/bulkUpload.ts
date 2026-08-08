3763479a457a875dbe56b2eda304a92ba9133dfa
M.gharaee <mahangharaee130@gmail.com>
2026-08-08 17:18:01 +0330

    کنترلر

--- apps/backend/presentation/webhost/controllers/bulkUpload.ts
+++ apps/backend/presentation/webhost/controllers/bulkUpload.ts
@@ -1,38 +1,54 @@
 import httpStatus from 'http-status';
 import type { Request, Response } from 'express';
+import ApiError from "#webhost/errors/apiError.js";
 
 
 class BulkUploadController {
     constructor() {};
 
     public async uploadCsvAndCreateBatch(req: Request, res: Response): Promise<any> {
-        const csvFile = req?.files?.file;
+        try {
+            const csvFile = req?.files?.file;
+        } catch (error) {
+            if (error instanceof ApiError) throw error;
+        }
     };
 
     public async listBatches(req: Request, res: Response): Promise<any> {};
 
     public async getBatchDetail(req: Request, res: Response): Promise<any> {
         /*const { batchId } = req.params;*/
-        const batchId = req.params.batchId;
+        try {
+            const batchId = req.params.batchId;
+        } catch (error) {
+            if (error instanceof ApiError) throw error;
+        }
     };
 
     public async updateBatchItems(req: Request, res: Response): Promise<any> {
         /*const { batchId } = req.params;
         const { items } = req.body;*/
-        const command = {
-            batchId: req.params.batchId,
-            items: req.body.items,
-        };
+        try {
+            const command = {
+                batchId: req.params.batchId,
+                items: req.body.items,
+            };
+        } catch (error) {
+            if (error instanceof ApiError) throw error;
+        }
     };
 
     public async deleteBatch(req: Request, res: Response): Promise<any> {
         /*const { batchId } = req.params;
         const deleteProducts = req.query.deleteProducts === "true";*/
-
-        const command = {
-            batchId: req.params.batchId,
-            deleteProducts: req.query.deleteProducts === "true",
-        };
+        try {
+            const command = {
+                batchId: req.params.batchId,
+                deleteProducts: req.query.deleteProducts === "true",
+            };
+        } catch (error) {
+            if (error instanceof ApiError) throw error;
+        }
     };
 }
 
