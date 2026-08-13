b354d9d2ef98bb82e758e8c975baaf3c1d6b28ef
m.gharaee <mahangharaee130@gmail.com>
2026-08-13 16:35:41 +0330

    اضافه کردن رابطه

--- apps/backend/infrastructure/repositories/MerchantRepository.ts
+++ apps/backend/infrastructure/repositories/MerchantRepository.ts
@@ -14,7 +14,11 @@ export default class MerchantRepository extends BaseRepository<Merchant> impleme
             include: {
                 merchantProducts: {
                     include: {
-                        product: true
+                        product: {
+                            include: {
+                                category: true
+                            }
+                        }
                     },
                 },
             },
