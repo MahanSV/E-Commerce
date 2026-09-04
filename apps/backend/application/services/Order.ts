5546fd2f2f48968bff43c5d01ffa7e76fe316cd0
m.gharaee <mahangharaee130@gmail.com>
2026-09-04 12:51:52 +0330

    نوتفیکیشن برای سفارشات

--- apps/backend/application/services/Order.ts
+++ apps/backend/application/services/Order.ts
@@ -33,12 +33,13 @@ export class OrderService implements OrderServiceInterface {
     async createCustomerOrder(command: createCustomerOrderCommand): Promise<CustomerOrderDTO> {
         const duplicateOrder = await this.orderRepository.findRecentDuplicateOrder(command.email, command.total);
 
-        if (duplicateOrder)
+        if (duplicateOrder) {
             throw new ApiError(
                 httpStatus.CONFLICT,
                 "An identical order was just created. Please wait a moment before creating another order with the same details.",
                 "Duplicate order detected"
-            );
+            ); // Returned Error
+        }
 
         const userEntity = UserFactory.createCustomer({
             name: command.name,
