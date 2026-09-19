3d675cce88e8cf7d6f9f9f28ff66a274e2b6c976
m.gharaee <mahangharaee130@gmail.com>
2026-09-19 16:44:52 +0330

    اضافه کردن مستندات

--- apps/backend/presentation/webhost/routes/customer_order_product.ts
+++ apps/backend/presentation/webhost/routes/customer_order_product.ts
@@ -3,37 +3,117 @@ import authenticate from '#middlewares/authenticityMiddlewares/authentication.ts
 import CustomerOrderProductController from '#webhost/controllers/customer_order_product.ts'
 import validate from "#middlewares/validation.ts";
 import {
-    createOrderProduct, deleteProductOrder, getProductOrder,
+    createOrderProduct,
+    createOrderProductDTOSchema,
+    deleteProductOrder,
+    getProductOrder,
+    orderGroupedDTOSchema,
+    orderProductDTOSchema,
     updateProductOrder
 } from "#webhost/validators/customer_order_product/customer_order_product.ts";
+import { createApiRouter } from '#webhost/docs/openApiRouter.ts';
+import {z} from "zod";
 
 const router = express.Router();
+const api = createApiRouter(router, '/order-product');
 
-router.get('/', /*authenticate,*/ CustomerOrderProductController.getAllProductOrders);
+api.get(
+    '/',
+    {
+        tags: ['order-product'],
+        responses: {
+            200: {
+                description: 'Group of product orders.',
+                content: { 'application/json': { schema: orderGroupedDTOSchema } }
+            }
+        }
+    },
+    // authenticate,
+    CustomerOrderProductController.getAllProductOrders
+);
 
-router.post(
+api.post(
     '/' ,
+    {
+        tags: ['order-product'],
+        request: {
+            body: {
+                content: {
+                    'application/json': {
+                        schema: createOrderProduct
+                    }
+                }
+            }
+        },
+        responses: {
+            201: {
+                description: 'add single product orders.',
+                content: { 'application/json': { schema: createOrderProductDTOSchema } }
+            }
+        }
+    },
     // authenticate,
     validate(createOrderProduct),
     CustomerOrderProductController.createOrderProduct
 );
 
-router.get(
+api.get(
     '/:id',
+    {
+        tags: ['order-product'],
+        request: {
+            params: z.object({ id: z.string() })
+        },
+        responses: {
+            200: {
+                description: 'Single product orders.',
+                content: { 'application/json': { schema: orderProductDTOSchema } }
+            }
+        }
+    },
     // authenticate,
     validate(getProductOrder),
     CustomerOrderProductController.getProductOrder
 );
 
-router.put(
+api.put(
     '/:id',
+    {
+        tags: ['order-product'],
+        request: {
+            params: z.object({ id: z.string() }),
+            body: {
+                content: {
+                    'application/json': {
+                        schema: updateProductOrder.omit(({ id: true }))
+                    }
+                }
+            }
+        },
+        responses: {
+            200: {
+                description: 'update product orders.',
+                content: { 'application/json': { schema: orderProductDTOSchema } }
+            }
+        }
+    },
     // authenticate,
     validate(updateProductOrder),
     CustomerOrderProductController.updateProductOrder
 );
 
-router.delete(
+api.delete(
     '/:id',
+    {
+        tags: ['order-product'],
+        request: {
+            params: z.object({ id: z.string() })
+        },
+        responses: {
+            204: { description: 'Delete product order.' },
+            404: { description: 'Order not found' }
+        }
+    },
     // authenticate,
     validate(deleteProductOrder),
     CustomerOrderProductController.deleteProductOrder
