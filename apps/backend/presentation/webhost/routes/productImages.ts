6b048328f0fd10ac7d07980f550ac6930dafd157
M.gharaee <mahangharaee130@gmail.com>
2026-09-20 14:59:22 +0330

    مستندات api های productImages.ts

--- apps/backend/presentation/webhost/routes/productImages.ts
+++ apps/backend/presentation/webhost/routes/productImages.ts
@@ -4,36 +4,170 @@ import ProductImageController from '#webhost/controllers/productImages.ts';
 import validate from "#middlewares/validation.ts";
 import {
     createImageSchema, deleteImageSchema,
-    getSingleProductImagesSchema,
+    getSingleProductImagesSchema, productImageDTOSchema,
     updateImageSchema
 } from "#webhost/validators/productImages/productImages.ts";
+import { createApiRouter } from '#webhost/docs/openApiRouter.ts';
+import { z } from 'zod';
+import {errorSchema} from "#webhost/validators/errorSchema.ts";
 
 
 const router = express.Router();
+const api  = createApiRouter(router, '/images');
 
-router.get(
+api.get(
     '/:id',
+    {
+        tags: ['Product Images'],
+        request: {
+            params: getSingleProductImagesSchema
+        },
+        responses: {
+            200: {
+                description: 'List of Product Images',
+                content: {
+                    'application/json': { schema: z.array(productImageDTOSchema).nullable() },
+                }
+            },
+            500: {
+                description: 'Internal Server Error',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(getSingleProductImagesSchema),
     ProductImageController.getSingleProductImages
 );
 
-router.post(
+api.post(
     '/',
+    {
+        tags: ['Product Images'],
+        request: {
+            body: {
+                content: {
+                    'application/json': {
+                        schema: createImageSchema
+                    }
+                }
+            }
+        },
+        responses: {
+            200: {
+                description: 'add product Image',
+                content: {
+                    'application/json': { schema: z.array(productImageDTOSchema).nullable() }
+                }
+            },
+            400: {
+                description: 'Bad Request',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+            500: {
+                description: 'Internal Server Error',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(createImageSchema),
     ProductImageController.createImage
 );
 
-router.put(
+api.put(
     '/:id',
+    {
+        tags: ['Product Images'],
+        request: {
+            params: updateImageSchema.pick({ id: true }),
+            body: {
+                content: {
+                    'application/json': {
+                        schema: updateImageSchema.omit({ id: true })
+                    }
+                }
+            }
+        },
+        responses: {
+            200: {
+                description: 'add product Image',
+                content: {
+                    'application/json': { schema: z.array(productImageDTOSchema).nullable() }
+                }
+            },
+            400: {
+                description: 'Bad Request',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                }
+            },
+            404: {
+                description: 'Not Found',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                }
+            },
+            500: {
+                description: 'Internal Server Error',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(updateImageSchema),
     ProductImageController.updateImage
 );
 
-router.delete(
+api.delete(
     '/:id',
+    {
+        tags: ['Product Images'],
+        request: {
+            params: deleteImageSchema
+        },
+        responses: {
+            204: {
+                description: 'Delete a product Image',
+            },
+            400: {
+                description: 'Bad Request',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                }
+            },
+            500: {
+                description: 'Internal Server Error',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(deleteImageSchema),
     ProductImageController.deleteImage
