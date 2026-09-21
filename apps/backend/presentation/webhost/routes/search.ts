1a3dbacc03e00937fc922d59a32ce2e02fb0eafc
M.gharaee <mahangharaee130@gmail.com>
2026-09-21 11:21:10 +0330

    مستندات api برای search.ts

--- apps/backend/presentation/webhost/routes/search.ts
+++ apps/backend/presentation/webhost/routes/search.ts
@@ -3,11 +3,41 @@ import authenticate from '#middlewares/authenticityMiddlewares/authentication.ts
 import SearchController from '#webhost/controllers/search.ts';
 import validate from "#middlewares/validation.ts";
 import {searchProductsSchema} from "#webhost/validators/search/search.ts";
+import { createApiRouter } from '#webhost/docs/openApiRouter.ts';
+import { z } from 'zod';
+import {productDTOSchema} from "#webhost/validators/products/products.ts";
+
 
 const router = express.Router();
+const api  = createApiRouter(router, '/search');
 
-router.get(
+// TODO: Test openapi doc's
+api.get(
     '/',
+    {
+        tags: ['Search products'],
+        request: {
+            query: searchProductsSchema
+        },
+        responses: {
+            200: {
+                description: 'Search products successfully.',
+                content: {
+                    'application/json': { schema:  z.array(productDTOSchema) }
+                }
+            },
+            500: {
+                description: 'Internal Server Error',
+                content: {
+                    'application/json': {
+                        schema: z.object({
+                            error: z.string()
+                        })
+                    }
+                }
+            }
+        }
+    },
     // authenticate,
     validate(searchProductsSchema),
     SearchController.searchProducts
