e40cec1e815bd824b1608a07c8d562d6c147faa8
M.gharaee <mahangharaee130@gmail.com>
2026-09-21 12:15:14 +0330

    مستندات api برای user.ts

--- apps/backend/presentation/webhost/routes/user.ts
+++ apps/backend/presentation/webhost/routes/user.ts
@@ -6,45 +6,178 @@ import {
     deleteUserSchema,
     getUserByEmailSchema,
     getUserSchema,
-    updateUserSchema
+    updateUserSchema,
+    simpleUserDTO,
+    userDTOSchema,
 } from "#webhost/validators/user/user.ts";
 import validate from "#middlewares/validation.ts";
-
+import { createApiRouter } from '#webhost/docs/openApiRouter.ts';
+import { z } from 'zod';
+import {errorSchema} from "#webhost/validators/errorSchema.ts";
 
 const router = express.Router();
-
-router.get('/', /*authenticate,*/ UserController.getAllUsers);
-
-router.post(
+const api = createApiRouter(router, '/users');
+// TODO: Test openapi doc's
+api.get(
     '/',
+    {
+        tags: ['Users'],
+        responses: {
+            200: {
+                description: 'List of all users',
+                content: {
+                    'application/json': { schema: z.array(simpleUserDTO) }
+                }
+            }
+        }
+    },
+    // authenticate,
+    UserController.getAllUsers
+);
+
+api.post(
+    '/',
+    {
+        tags: ['Users'],
+        request: {
+            body: {
+                content: {
+                    'application/json': { schema: createUserSchema }
+                }
+            }
+        },
+        responses: {
+            201: {
+                description: 'add user',
+                content: {
+                    'application/json': { schema: z.array(simpleUserDTO) }
+                }
+            }
+        }
+    },
     // authenticate,
     validate(createUserSchema),
     UserController.createUser
 );
 
-router.get(
+api.get(
     '/:id',
+    {
+        tags: ['Users'],
+        request: {
+            params: getUserSchema
+        },
+        responses: {
+            200: {
+                description: 'Single user',
+                content: {
+                    'application/json': { schema: simpleUserDTO }
+                }
+            },
+            404: {
+                description: 'Not Found',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(getUserSchema),
     UserController.getUser
 );
 
-router.put(
+api.put(
     '/:id',
+    {
+        tags: ['Users'],
+        request: {
+            params: updateUserSchema.pick({ id: true }),
+            body: {
+                content: {
+                    'application/json': {
+                        schema: updateUserSchema.omit({ id: true })
+                    }
+                }
+            }
+        },
+        responses: {
+            200: {
+                description: 'Update user',
+                content: {
+                    'application/json': { schema: userDTOSchema }
+                }
+            },
+            400: {
+                description: 'Bad Request',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(updateUserSchema),
     UserController.updateUser
 );
 
-router.delete(
+api.delete(
     '/:id',
+    {
+        tags: ['Users'],
+        request: {
+            params: deleteUserSchema
+        },
+        responses: {
+            200: {
+                description: 'Delete user',
+                content: {
+                    'application/json': { schema: userDTOSchema }
+                }
+            },
+            400: {
+                description: 'Bad Request',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(deleteUserSchema),
     UserController.deleteUser
 );
 
-router.get(
+api.get(
     '/email/:email',
+    {
+        tags: ['Users'],
+        request: {
+            params: getUserByEmailSchema
+        },
+        responses: {
+            200: {
+                description: 'get user by email',
+                content: {
+                    'application/json': { schema: userDTOSchema }
+                }
+            },
+            404: {
+                description: 'Not Found',
+                content: {
+                    'application/json': {
+                        schema: errorSchema
+                    }
+                },
+            },
+        }
+    },
     // authenticate,
     validate(getUserByEmailSchema),
     UserController.getUserByEmail
